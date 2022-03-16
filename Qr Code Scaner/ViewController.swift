5d54202b23da20bfcee2723510b891c11da83d6a
Aleks.sinya <sinyagovskiy93@mail.ru>
2022-03-16 18:44:14 +0700

    Initial Commit

---
+++ Qr Code Scaner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Qr Code Scaner
+//
+//  Created by Алексей Синяговский on 16.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
